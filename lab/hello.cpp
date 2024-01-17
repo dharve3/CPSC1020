06fd9ec1e3f0382c8f1f0269d6471b3c7ad5efe7
DylanJoey17 <dharve3@clemson.edu>
2024-01-17 09:54:59 -0500

    Added hello world and lab template

---
+++ lab/hello.cpp
@@ -0,0 +1,14 @@
+// My Name: Dylan Harvey
+// My Class: CPSC 1021
+// Date: 1/17/2024
+// Desc: Prints "HELLO CLEMSON" to the terminal output
+// Time: 5 minutes
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "HELLO CLEMSON";
+	return 0;
+}
---
+++ lab/template.cpp
@@ -0,0 +1,14 @@
+// My Name: your name goes here
+// My Class: CPSC 1021 (for labs) or CPSC 1020 (for lecture class)
+// Date: Date you wrote the program
+// Desc: brief description of what the program does
+// Time: approximate amount of time you spent on this program
+
+#include <iostream>
+
+using namespace std;
+
+int main( ) {
+	cout << "Hello World\n";
+	return 0;
+}
