3a628d714ec00f6af9e537c07a07bd20eb365f09
D-Guy Laptop <ddcool535@gmail.com>
2024-01-18 09:49:17 -0500

    Added week2 and week3 lecture example files

---
+++ lecture/week2/hello_user.cpp
@@ -0,0 +1,17 @@
+// A simple C++ program that greets the user
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string fname, lname;
+  cout << "Hello, user! Please enter your first name: ";
+  cin >> fname;
+  cout << "Please enter your last name: ";
+  cin >> lname;
+
+  cout << "Hi, " << fname << " " << lname << "! " << "Nice to meet you!" << endl;
+  
+  return 0;
+}
---
+++ lecture/week2/hello_world.cpp
@@ -0,0 +1,10 @@
+// A simple hello world C++ program 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello, Word!\n";
+  
+  return 0;
+}
