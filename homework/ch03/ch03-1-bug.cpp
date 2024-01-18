61be6f1bea4698cc8e7939dd499c1c96df20a4c8
D-Guy Laptop <ddcool535@gmail.com>
2024-01-18 10:00:37 -0500

    Added inital ch03-bug programs

---
+++ homework/ch03/ch03-1-bug.cpp
@@ -0,0 +1,21 @@
+// This program illustrates a problem that can occur if
+// cin is used to read character data into a string object.
+// input a name with first and last (space between) and observe the bug, then fix
+#include <iostream>
+#include <string>       // Header file needed to use string objects
+using namespace std;
+
+int main()
+{
+    string name;
+    string city;
+
+    cout << "Please enter your name: ";
+    cin  >> name;
+    cout << "Enter the city you live in: ";
+    cin  >> city;
+
+    cout << "Hello, " << name << endl;
+    cout << "You live in " << city << endl;
+    return 0;
+}
---
+++ homework/ch03/ch03-3-bug.cpp
@@ -0,0 +1,16 @@
+// This program prints a character from its ASCII code.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 65;
+
+    // Display the value of the number variable
+    cout << number << endl;
+
+    // Needs a type cast to display the value of number
+    // converted to the char data type
+    cout << number << endl;
+    return 0;
+}
