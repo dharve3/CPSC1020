819290737314b050476a96b75a31096f59ad9e26
D-Guy Laptop <ddcool535@gmail.com>
2024-01-23 10:16:14 -0500

    Completed compound calculator

--- lab/lab1/compoundCalc.cpp
+++ lab/lab1/compoundCalc.cpp
@@ -1,13 +1,37 @@
 // My Name: Dylan Harvey
 // My Class: CPSC 1021
 // Date: 1/22/2024
-// Desc: 
-// Time: 
+// Desc: Calculates compound interest from user input
+// Time: ~45 minutes
 
-#include <iostream>
+#include "compoundCalc.h"
 
-using namespace std;
+// TEMP: RS = P x (1 + r)t aka RS = P * pow((1 + r), t)
+// compoundCalc: Calculates the retirement savings given principal, rate, and time
+float compoundCalc(float p, float r, float t) {
+    float rs;
 
+    // Not calculating decimals properly
+    rs = p * pow((1 + r), t);
+
+    return rs;
+}
+
+// Using main-last structure
 int main() {
-    
+    float principal, rate, years, savings;
+
+    // Get values from user
+    cout << "Enter your principal (current value): ";
+    cin >> principal;
+    cout << "Enter the interest rate as a decimal (e.g. 0.025): ";
+    cin >> rate;
+    cout << "Enter the number of years: ";
+    cin >> years;
+
+    savings = compoundCalc(principal, rate, years);
+
+    cout << "Your retirement savings will be " << setprecision(2) << fixed << savings << endl;
+
+    return 0;
 }
---
+++ lab/lab1/compoundCalc.h
@@ -0,0 +1,10 @@
+#ifndef COMPOUNDCALC_H
+#define COMPOUNDCALC_H
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+#endif
