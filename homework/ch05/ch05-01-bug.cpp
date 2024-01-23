a3059a954609c7473ace30891b62990e20a413dd
D-Guy Laptop <ddcool535@gmail.com>
2024-01-23 09:41:47 -0500

    Added bug files for ch04-06

---
+++ homework/ch05/ch05-01-bug.cpp
@@ -0,0 +1,15 @@
+// This program demonstrates a simple while loop.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int count = 1;
+
+   while (count <= 5)
+   {
+      cout << "Hello   ";
+   }
+   cout << "\nThat's all!\n";
+   return 0;
+}
