320fb0075f5c9c3d66f4733f84f0ea58b0c345f2
D-Guy Laptop <ddcool535@gmail.com>
2024-01-25 17:24:41 -0500

    Updated templates

---
+++ templates/headerfile.h
@@ -0,0 +1,15 @@
+#ifndef COMPOUNDCALC_H
+#define COMPOUNDCALC_H
+
+// Include statements
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+// Using...
+using namespace std;
+
+// Function prototypes
+float compoundCalc(float inital_principal, float interest_rate, float time_years);
+
+#endif
