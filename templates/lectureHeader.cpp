c6aa660a8c0896bac1f48324a39387d2a9a09bf0
D-Guy Laptop <ddcool535@gmail.com>
2024-02-13 09:50:21 -0500

    Updated lecture template header

--- templates/lectureHeader.cpp
+++ templates/lectureHeader.cpp
@@ -3,4 +3,4 @@
 // Date: Due Date
 // <Assignment name here>
 // Time: (total time invested in the entire project)
-// Description: brief description of file contents
+// Desc: brief description of file contents
