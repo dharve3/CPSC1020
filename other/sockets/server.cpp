eca3db7b8ca4a89272d27df035e02e8e681031f7
D-Guy Desktop <ddcool535@gmail.com>
2024-02-14 23:30:37 -0500

    Reverted address to inaddr_any

--- other/sockets/server.cpp
+++ other/sockets/server.cpp
@@ -19,7 +19,7 @@ int main() {
     sockaddr_in serverAddress; // Data type to store the address of the socket
     serverAddress.sin_family = AF_INET; // 
     serverAddress.sin_port = htons(8080); // Convert unsigned int from machine byte to network byte
-    serverAddress.sin_addr.s_addr = INADDR_LOOPBACK; // INADDR_ANY = No particular IP, listen to all available IPs; INADDR_LOOPBACK = localhost (127.0.0.1)
+    serverAddress.sin_addr.s_addr = INADDR_ANY; // INADDR_ANY = No particular IP, listen to all available IPs; INADDR_LOOPBACK = localhost (127.0.0.1)
     // Use inet_addr(IP) for specific addresses to convert to 32-bit binary rep in network byte order
     // Use inet_pton(AF_INET, ipAddress, &(serverAddress.sin_addr)) for a more modern approach
 
