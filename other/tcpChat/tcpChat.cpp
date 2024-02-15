7ad64da7b443d46b89392440b9dd216c53a260ad
D-Guy Desktop <ddcool535@gmail.com>
2024-02-14 21:42:28 -0500

    First iteration p2p chat

---
+++ other/tcpChat/tcpChat.cpp
@@ -0,0 +1,65 @@
+// Insert respective header here
+
+#include "tcpChat.h"
+
+TCPChat::TCPChat() {
+    // Create socket
+    socket_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_ == -1) {
+        std::cerr << "Error: Failed to create socket\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+TCPChat::~TCPChat() {
+    // Close socket
+    close(socket_);
+}
+
+void TCPChat::startChat(cosnt std::string& peerIP, int peerPort) {
+    serverAddr_.sin_family = AF_INET;
+    serverAddr_.sin_port = htons(peerPort);
+    serverAddr_.sin_addr.s_addr = inet_addr(peerIP.c_str());
+
+    // Connect to peer
+    if (connect(socket_, (struct sockaddr *)&serverAddr_, sizeof(serverAddr_)) == -1) {
+        std::cerr << "Error: Connection failed\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::thread recvThread(&TCPChat::recieveMessage, this);
+    recvThread.detach(); // Detach thread to run independently
+
+    std::string message;
+    while (true) {
+        std::getline(std::cin, message);
+        sendMessage(message);
+    }
+}
+
+void TCPChat::sendMessage(const std::string& message) {
+    send(socket_, message.c_str(), message.size(), 0);
+}
+
+void TCPChat::recieveMessage() {
+    char buffer[BUFFER_SIZE];
+    while (true) {
+        ssize_t bytesReceived = recv(socket_, buffer, BUFFER_SIZE, 0);
+        if (bytesReceived > 0) {
+            buffer[bytesReceived] = "\0"; // Null-termniate the received data
+            std::cout << "Received: " << buffer << std::endl;
+        } else if (bytesReceived == 0) {
+            std::cout << "Peer disconnected\n";
+            break;
+        } else {
+            std::cerr << "Error: Failed to receive message\n";
+            break;
+        }
+    }
+}
+
+int main( ) {
+    TCPChat chat;
+    chat.startChat("127.0.0.1", 12345); 
+	return 0;
+}
---
+++ other/tcpChat/tcpChat.h
@@ -0,0 +1,35 @@
+#ifndef TCPCHAT_H
+#define TCPCHAT_H
+
+#include <iostream>
+#include <string> // C++ strings
+#include <thread> // Multi-threading
+#include <cstring> // String manipulation
+#include <cstdlib> // exit() function
+#include <unistd.h> // UNIX standard functions
+
+#include <arpa/inet.h> // manipulate IP addresses
+#include <sys/socket.h> // Sockets
+#include <netinet/in.h> // Defining internet domain addrs
+
+// Constants
+// const int PORT = 12345;
+// const int BUFFER_SIZE = 1024;
+// const std::string IP = "127.0.0.1";
+
+// Class Declaration
+class TCPChat {
+public:
+    TCPChat();
+    ~TCPChat();
+    void startChat(const std::string& peerIP, int peerPort);
+    void sendMessage(const std::string& string& message);
+    void recieveMessage();
+
+private:
+    int socket_;
+    struct sockaddr_in serverAddr_;
+    const int BUFFER_SIZE = 1024;
+};
+
+#endif // TCPCHAT_H
