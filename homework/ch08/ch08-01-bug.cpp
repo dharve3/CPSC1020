1838fb7d81e738eeb2009d5bba76636e436d9016
D-Guy Laptop <ddcool535@gmail.com>
2024-02-20 09:54:00 -0500

    Fixed ch08-01-bug

--- homework/ch08/ch08-01-bug.cpp
+++ homework/ch08/ch08-01-bug.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-    const int NUM_MONTHS = 10;
+    const int NUM_MONTHS = 12; // Changed const to 12 to fit array size
     int choice;
     string name[NUM_MONTHS] =
                { "January",   "February", "March",    "April",
@@ -23,15 +23,15 @@ int main()
          << "in any month.\n\n";
 
     // Display the months
-    for (int month = 1; month <= NUM_MONTHS; month--)
-        cout << setw(2) << month << "  " << name[month] << endl;
+    for (int month = 1; month <= NUM_MONTHS; month++) // Changed to increment instead of decrement
+        cout << setw(2) << month << "  " << name[month-1] << endl; // Access array with month-1
 
     cout << "\nEnter the number of the month you want: ";
     cin  >> choice;
 
     // Use the choice the user entered to get the name of
     // the month and its number of days from the arrays.
-    cout << "The month of " << name[choice] << " has "
-         << days[choice]  << " days.\n";
+    cout << "The month of " << name[choice-1] << " has " // Access array with choice-1
+         << days[choice-1]  << " days.\n"; // Access array with choice-1
     return 0;
 }
