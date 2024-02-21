0ee45dc5707e9e0d0f8593292fb9a819351d95ea
D-Guy Desktop <ddcool535@gmail.com>
2024-02-21 14:01:51 -0500

    Wrote Motorcycles class

--- lab/lab05/Motorcycles.cpp
+++ lab/lab05/Motorcycles.cpp
@@ -4,3 +4,80 @@
 // Lab Name: Lab05 - Parking Permits
 // File Name: Motorcycles.cpp
 // Desc: Description here
+
+#include "Motorcycles.h"
+
+// Validates that input is not empty for the string type
+bool Motorcycles::validateInput(string v) {
+    if (v.empty())
+        return 0;
+    else
+        return 1;
+}
+
+// Validates that input is greater than or equal to 0 for int type
+bool Motorcycles::validateInput(int v) {
+    if (v < 0)
+        return 0;
+    else
+        return 1;
+}
+
+// Mutators (Setters)
+bool Motorcycles::setMake(string m) {
+    if (validateInput(m)) {
+        make = m;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Motorcycles::setModel(string o) {
+    if (validateInput(o)) {
+        model = o;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Motorcycles::setCC(int c) {
+    if (validateInput(c)) {
+        cc = c;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Motorcycles::setCapacity(int p) {
+    if (validateInput(p)) {
+        capacity = p;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Motorcycles::setYear(int y) {
+    if (validateInput(y)) {
+        year = y;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Accessors (Getters)
+string Motorcycles::getMake() {
+    return make;
+}
+string Motorcycles::getModel() {
+    return model;
+}
+int Motorcycles::getCC() {
+    return cc;
+}
+int Motorcycles::getCapacity() {
+    return capacity;
+}
+int Motorcycles::getYear() {
+    return year;
+}
--- lab/lab05/Motorcycles.h
+++ lab/lab05/Motorcycles.h
@@ -4,3 +4,45 @@
 // Lab Name: Lab05 - Parking Permits
 // File Name: Motorcycles.h
 // Desc: Description here
+
+#ifndef MOTORCYCLES_H
+#define MOTORCYCLES_H
+
+#include <string>
+
+using namespace std;
+
+class Visitors
+{
+private:
+    string make;
+    string model;
+    int cc;
+    int capacity;
+    int year;
+public:
+    // Constructors
+    Motorcycles() = default; // Default constructor
+    Motorcycles(string m, string o, int c, int p, int y): make(m), model(o), cc(c), capacity(p), year(y) { } // Constructor for all values given
+
+    // Input Validation Methods
+    bool validateInput(string v)
+    bool validateInput(int v)
+
+    // Mutators (Setters)
+    bool setMake(string m)
+    bool setModel(string o)
+    bool setCC(int c)
+    bool setCapacity(int p)
+    bool setYear(int y)
+
+    // Accessors (Getters)
+    string getMake()
+    string getModel()
+    string getColor()
+    string getLicensePlate()
+    int getYear()
+
+}
+
+#endif // MOTORCYCLES_H
