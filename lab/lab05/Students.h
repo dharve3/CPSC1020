a02f99055af793e39af5c032c641f4f775f481af
jbhosal <157389465+jbhosal@users.noreply.github.com>
2024-02-25 16:00:19 -0500

    Update Students.h

--- lab/lab05/Students.h
+++ lab/lab05/Students.h
@@ -1,6 +1,47 @@
-// Name: Dylan Harvey
-// Date: 03/01/2024
-// Class: CPSC 1021
-// Lab Name: Lab05 - Parking Permits
-// File Name: Students.h
-// Desc: Description here
+// Name: Janaki Bhosale
+// Date: 3/1/24
+// CPSC 1021
+// Lab 5: Parking Permits
+// File: Students.h
+// Desc: Header file containing Student class, including atrributes and methods
+
+#ifndef STUDENTS_H
+#define STUDENTS_H
+
+#include <string>
+
+using namespace std;
+
+class Students {
+    private:
+        string name;
+        string email;
+        string address;
+        int studentID;
+        int educationLevel;
+    public:
+        // constructors
+        Students(): name(""), email(""), address(""), studentID(0), educationLevel(0) {}
+        Students(string n, string e, string a, int sID, int l): name(n), email(e), address(a), studentID(sID), eductionLevel(l) {}
+
+        // Input Validation Methods
+        bool validateInput(string v);
+	      bool validateInput(int v);
+
+	      // Mutators
+	      bool setName(string n)
+	      bool setEmail(string e);
+	      bool setAddress(string a);
+	      bool setStudentID(int sID);
+	      bool setEducationLevel(int l);
+
+	      // Accessors
+	      string getName();
+	      string getEmail();
+	      string getAddress();
+	      int getstudentID();
+	      int getEducationLevel();
+
+}
+
+#endif // STUDENTS_H
