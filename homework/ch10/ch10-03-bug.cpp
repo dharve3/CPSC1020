c25c471181e57ad3cfe2b3f3342721d10ca1083a
D-Guy Laptop <ddcool535@gmail.com>
2024-02-26 20:46:05 -0500

    Fixed ch10-03-bug

--- homework/ch10/ch10-03-bug.cpp
+++ homework/ch10/ch10-03-bug.cpp
@@ -10,22 +10,22 @@ int main()
 
    // Display the contents of x, y, and z.
     cout << "Here are the values of x, y, and z:\n";
-    cout << &x << "  " << &y << "  " << &z << endl;
+    cout << x << "  " << y << "  " << z << endl; // directly displaying
 
    // Use the pointer to manipulate x, y, and z
 
-    ptr = x;         // Store the address of x in ptr
-    ptr *= 2;        // Multiply value in x by 2
+    ptr = &x;         // Store the address of x in ptr // Added &
+    *ptr *= 2;        // Multiply value in x by 2 // Added * before ptr
 
     ptr = &y;         // Store the address of y in ptr
-    ptr *= 2;        // Multiply value in y by 2
+    *ptr *= 2;        // Multiply value in y by 2 // Added * before ptr
 
     ptr = &z;         // Store the address of z in ptr
-    ptr *= 2;        // Multiply value in z by 2
+    *ptr *= 2;        // Multiply value in z by 2 // Added * before ptr
 
     // Display the contents of x, y, and z
     cout << "Once again, here are the values "
          << "of x, y, and z:\n";
-    cout << *x << "  " << *y << "  " << *z << endl;
+    cout << x << "  " << y << "  " << z << endl; // directly displaying
     return 0;
 }
