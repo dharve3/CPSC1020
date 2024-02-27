e67e3dc38912df9f2cf2a00d6937830e5e59a9cb
D-Guy Laptop <ddcool535@gmail.com>
2024-02-27 09:42:59 -0500

    Fixed customer ordering, formatting, and syntax

--- lab/lab05/Customers.cpp
+++ lab/lab05/Customers.cpp
@@ -1,179 +1,14 @@
-//Name: Angie Diaz
-//Date: 3/1/24
-//CPSC 1021
-//Lab 5: Parking Permits
-//File: Employees.cpp
-//Desc: File containing Employee class method definitions
+// Customers.cpp
 
-#include "Employees.h"
+#include "Customers.h"
 
-// Validates that input is not empty for the string type
-bool Employees::validateInput(string v) {
-    if (v.empty())
-        return 0;
-    else
-        return 1;
-}
-
-// Validates that input is greater than or equal to 0 for int type
-bool Employees::validateInput(int v) {
-    if (v < 0)
-        return 0;
-    else
-        return 1;
-}
-
-// Mutators (Setters)
-bool Employees::setName(string n) {
-    if (validateInput(n)) {
-        name = n;
-        return 1;
-    } else {
-        return 0;
-    }
-}
-bool Employees::setEmail(string e) {
-    if (validateInput(e)) {
-        email = e;
-        return 1;
-    } else {
-        return 0;
-    }
-}
-bool Employees::setAddress(string a) {
-    if (validateInput(a)) {
-        address = a;
-        return 1;
-    } else {
-        return 0;
-    }
-}
-bool Employees:: setEmployeeID(int d) {
-	if (validateInput(d)) {
-		employeeID = d;
-		return 1;
-	} else {
-		return 0;
-	}
-}
-bool Employees:: setYearsEmployed(int y) {
-    if (validateInput(y)) {
-        yearsEmployed = y;
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-// Accessors (Getters)
-string Employees::getName() {
-    return name;
-}
-string Employees::getEmail() {
-    return email;
-}
-string Employees::getAddress() {
-    return address;
-}
-int Employees::getEmployeeID() {
-    return employeeID;
-}
-int Employees::getYearsEmployed() {
-    return yearsEmployed;
-}
-
-// Name: Janaki Bhosale
-// Date: 3/1/24
-// CPSC 1021
-// Lab 5: Parking Permits
-// File: Students.cpp
-// Desc: File containing Student class definitons and functions
-
-#include "Students.h"
-
-// Validates that input is not empty for the string type
-bool Students::validateInput(string v) {
-    if (v.empty())
-        return 0;
-    else
-        return 1;
-}
-
-// Validates that input is greater than or equal to 0 for int type
-bool Students::validateInput(int v) {
-    if (v < 0)
-        return 0;
-    else
-        return 1;
-}
-
-// Mutators (Setters)
-bool Students::setName(string n) {
-    if (validateInput(n)) {
-        name = n;
-        return 1;
-    } else {
-        return 0;
-    }
-}
-bool Students::setEmail(string e) {
-    if (validateInput(e)) {
-        email = e;
-        return 1;
-    } else {
-        return 0;
-    }
-}
-bool Students::setAddress(string a) {
-    if (validateInput(a)) {
-        address = a;
-        return 1;
-    } else {
-        return 0;
-    }
-}
-bool Students:: setStudentID(int sID) {
-	if (validateInput(d)) {
-		studentID = d;
-		return 1;
-	} else {
-		return 0;
-	}
-}
-bool Students:: setEducationLevel(int l) {
-    if (validateInput(l)) {
-        educationLevel = l;
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-// Accessors (Getters)
-string Students::getName() {
-    return name;
-}
-string Students::getEmail() {
-    return email;
-}
-string Students::getAddress() {
-    return address;
-}
-int Students::getStudentID() {
-    return studentID;
-}
-int Students::getEducationLevel() {
-    return educationLevel;
-}
 
 // Name: Dylan Harvey
 // Date: 03/01/2024
 // Class: CPSC 1021
 // Lab Name: Lab05 - Parking Permits
-// File Name: Visitors.cpp
+// File Name: Customers.cpp
 // Desc: Source file for Visitors Class (Type of customer), declares methods
-
-#include "Visitors.h"
 
 // Validates that input is not empty for the string type
 bool Visitors::validateInput(string v) {
@@ -256,4 +91,171 @@ int Visitors::getRegNumber() {
 }
 bool Visitors::getFirstVisit() {
     return firstVisit;
+}
+
+
+// Name: Janaki Bhosale
+// Date: 3/1/24
+// CPSC 1021
+// Lab 5: Parking Permits
+// File: Customers.cpp
+// Desc: File containing Student class definitons and functions
+
+
+// Validates that input is not empty for the string type
+bool Students::validateInput(string v) {
+    if (v.empty())
+        return 0;
+    else
+        return 1;
+}
+
+// Validates that input is greater than or equal to 0 for int type
+bool Students::validateInput(int v) {
+    if (v < 0)
+        return 0;
+    else
+        return 1;
+}
+
+// Mutators (Setters)
+bool Students::setName(string n) {
+    if (validateInput(n)) {
+        name = n;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Students::setEmail(string e) {
+    if (validateInput(e)) {
+        email = e;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Students::setAddress(string a) {
+    if (validateInput(a)) {
+        address = a;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Students:: setStudentID(int sID) {
+	if (validateInput(d)) {
+		studentID = d;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+bool Students:: setEducationLevel(int l) {
+    if (validateInput(l)) {
+        educationLevel = l;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Accessors (Getters)
+string Students::getName() {
+    return name;
+}
+string Students::getEmail() {
+    return email;
+}
+string Students::getAddress() {
+    return address;
+}
+int Students::getStudentID() {
+    return studentID;
+}
+int Students::getEducationLevel() {
+    return educationLevel;
+}
+
+
+//Name: Angie Diaz
+//Date: 3/1/24
+//CPSC 1021
+//Lab 5: Parking Permits
+//File: Customers.cpp
+//Desc: File containing Employee class method definitions
+
+// Validates that input is not empty for the string type
+bool Employees::validateInput(string v) {
+    if (v.empty())
+        return 0;
+    else
+        return 1;
+}
+
+// Validates that input is greater than or equal to 0 for int type
+bool Employees::validateInput(int v) {
+    if (v < 0)
+        return 0;
+    else
+        return 1;
+}
+
+// Mutators (Setters)
+bool Employees::setName(string n) {
+    if (validateInput(n)) {
+        name = n;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Employees::setEmail(string e) {
+    if (validateInput(e)) {
+        email = e;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Employees::setAddress(string a) {
+    if (validateInput(a)) {
+        address = a;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+bool Employees:: setEmployeeID(int d) {
+	if (validateInput(d)) {
+		employeeID = d;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+bool Employees:: setYearsEmployed(int y) {
+    if (validateInput(y)) {
+        yearsEmployed = y;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Accessors (Getters)
+string Employees::getName() {
+    return name;
+}
+string Employees::getEmail() {
+    return email;
+}
+string Employees::getAddress() {
+    return address;
+}
+int Employees::getEmployeeID() {
+    return employeeID;
+}
+int Employees::getYearsEmployed() {
+    return yearsEmployed;
 }
--- lab/lab05/Customers.h
+++ lab/lab05/Customers.h
@@ -1,9 +1,4 @@
-//Name: Angie Diaz
-//Date: 3/1/24
-//CPSC 1021
-//Lab 5: Parking Permits
-//File: Employees.h
-//Desc: Header file containing Employee class atrributes and methods
+// Customers.h
 
 #ifndef CUSTOMERS_H
 #define CUSTOMERS_H
@@ -11,6 +6,93 @@
 #include <string>
 
 using namespace std;
+
+
+// Name: Dylan Harvey
+// Date: 03/01/2024
+// Class: CPSC 1021
+// Lab Name: Lab05 - Parking Permits
+// File Name: Customers.h
+// Desc: Headerfile for Visitors Class (Type of customer), defines methods
+
+class Visitors 
+{
+private:
+    string name;
+    string email;
+    string address;
+    int regNumber;
+    bool firstVisit;
+public:
+    // Constructors
+    Visitors(): name(""), email(""), address(""), regNumber(0), firstVisit(0) { } // Default constructor
+    Visitors(string n, string e, string a, int rN, bool fV): name(n), email(e), address(a), regNumber(rN), firstVisit(fV) { } // Constructor for all values given
+
+    // Input Validation Methods
+    bool validateInput(string v);
+    bool validateInput(int v);
+
+    // Mutators (Setters)
+    bool setName(string n);
+    bool setEmail(string e);
+    bool setAddress(string a);
+    bool setRegNumber(int rN);
+    bool setFirstVisit(bool fV);
+
+    // Accessors (Getters)
+    string getName();
+    string getEmail();
+    string getAddress();
+    int getRegNumber();
+    bool getFirstVisit();
+};
+
+
+// Name: Janaki Bhosale
+// Date: 3/1/24
+// CPSC 1021
+// Lab 5: Parking Permits
+// File: Customers.h
+// Desc: Header file containing Student class, including atrributes and methods
+
+class Students {
+    private:
+        string name;
+        string email;
+        string address;
+        int studentID;
+        int educationLevel;
+    public:
+        // constructors
+        Students(): name(""), email(""), address(""), studentID(0), educationLevel(0) {}
+        Students(string n, string e, string a, int sID, int l): name(n), email(e), address(a), studentID(sID), eductionLevel(l) {}
+
+        // Input Validation Methods
+        bool validateInput(string v);
+	      bool validateInput(int v);
+
+	      // Mutators
+	      bool setName(string n)
+	      bool setEmail(string e);
+	      bool setAddress(string a);
+	      bool setStudentID(int sID);
+	      bool setEducationLevel(int l);
+
+	      // Accessors
+	      string getName();
+	      string getEmail();
+	      string getAddress();
+	      int getstudentID();
+	      int getEducationLevel();
+};
+
+
+//Name: Angie Diaz
+//Date: 3/1/24
+//CPSC 1021
+//Lab 5: Parking Permits
+//File: Customers.h
+//Desc: Header file containing Employee class atrributes and methods
 
 class Employees 
 {
@@ -44,83 +126,5 @@ public:
 	int getYearsEmployed();
 };
 
-// Name: Janaki Bhosale
-// Date: 3/1/24
-// CPSC 1021
-// Lab 5: Parking Permits
-// File: Students.h
-// Desc: Header file containing Student class, including atrributes and methods
-
-class Students {
-    private:
-        string name;
-        string email;
-        string address;
-        int studentID;
-        int educationLevel;
-    public:
-        // constructors
-        Students(): name(""), email(""), address(""), studentID(0), educationLevel(0) {}
-        Students(string n, string e, string a, int sID, int l): name(n), email(e), address(a), studentID(sID), eductionLevel(l) {}
-
-        // Input Validation Methods
-        bool validateInput(string v);
-	      bool validateInput(int v);
-
-	      // Mutators
-	      bool setName(string n)
-	      bool setEmail(string e);
-	      bool setAddress(string a);
-	      bool setStudentID(int sID);
-	      bool setEducationLevel(int l);
-
-	      // Accessors
-	      string getName();
-	      string getEmail();
-	      string getAddress();
-	      int getstudentID();
-	      int getEducationLevel();
-
-}
-
-// Name: Dylan Harvey
-// Date: 03/01/2024
-// Class: CPSC 1021
-// Lab Name: Lab05 - Parking Permits
-// File Name: Visitors.h
-// Desc: Headerfile for Visitors Class (Type of customer), defines methods
-
-class Visitors 
-{
-private:
-    string name;
-    string email;
-    string address;
-    int regNumber;
-    bool firstVisit;
-public:
-    // Constructors
-    Visitors(): name(""), email(""), address(""), regNumber(0), firstVisit(0) { } // Default constructor
-    Visitors(string n, string e, string a, int rN, bool fV): name(n), email(e), address(a), regNumber(rN), firstVisit(fV) { } // Constructor for all values given
-
-    // Input Validation Methods
-    bool validateInput(string v);
-    bool validateInput(int v);
-
-    // Mutators (Setters)
-    bool setName(string n);
-    bool setEmail(string e);
-    bool setAddress(string a);
-    bool setRegNumber(int rN);
-    bool setFirstVisit(bool fV);
-
-    // Accessors (Getters)
-    string getName();
-    string getEmail();
-    string getAddress();
-    int getRegNumber();
-    bool getFirstVisit();
-
-};
 
 #endif // CUSTOMERS_H
