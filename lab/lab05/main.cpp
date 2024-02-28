7b1e9dfbde176d2eb0a445fddc4217699e04960b
D-Guy Desktop <ddcool535@gmail.com>
2024-02-28 12:11:35 -0500

    Updated input to use getline, added cout to printInvoice calls

--- lab/lab05/main.cpp
+++ lab/lab05/main.cpp
@@ -30,11 +30,11 @@ int main() {
     
     // Get customer type from user
     cout << "What type of customer are you? (visitor, student, or employee?): ";
-    cin >> customerType;
+    getline(cin, customerType);
     // Loop until valid input is recieved
     while (customerType != "visitor" && customerType != "student" && customerType != "employee") {
         cout << "Please try again (input is case sensitive): ";
-        cin >> customerType;
+        getline(cin, customerType);
     }
 
     if (customerType == "visitor") {
@@ -44,15 +44,15 @@ int main() {
 
         // Get input from user
         cout << "Enter your name: ";
-        cin >> name;
+        getline(cin, name);
         cout << "Enter your email: ";
-        cin >> email;
+        getline(cin, email);
         cout << "Enter your address: ";
-        cin >> address;
+        getline(cin, address);
         cout << "Enter your registration number: ";
-        cin >> regNum;
+        getline(cin, regNum);
         cout << "Is this your first visit? (y/n): ";
-        cin >> input;
+        getline(cin, input);
         // Logic for first visit
         if (input == "y") {
             firstVisit = 1;
@@ -69,15 +69,15 @@ int main() {
 
         // Get input from user
         cout << "Enter your name: ";
-        cin >> name;
+        getline(cin, name);
         cout << "Enter your email: ";
-        cin >> email;
+        getline(cin, email);
         cout << "Enter your address: ";
-        cin >> address;
+        getline(cin, address);
         cout << "Enter your studentID: ";
-        cin >> studentId;
+        getline(cin, studentId);
         cout << "Enter your education level (years): ";
-        cin >> educationLevel;
+        getline(cin, educationLevel);
 
         // Create Student object
         student1 = Student(name, email, address, studentId, educationLevel);
@@ -87,15 +87,15 @@ int main() {
 
         // Get input from user
         cout << "Enter your name: ";
-        cin >> name;
+        getline(cin, name);
         cout << "Enter your email: ";
-        cin >> email;
+        getline(cin, email);
         cout << "Enter your address: ";
-        cin >> address;
+        getline(cin, address);
         cout << "Enter your employeeID: ";
-        cin >> employeeId;
+        getline(cin, employeeId);
         cout << "Enter your years employed: ";
-        cin >> yearsEmployed;
+        getline(cin, yearsEmployed);
 
         // Create Employee object
         employee1 = Employee(name, email, address, employeeId, yearsEmployed);
@@ -104,11 +104,11 @@ int main() {
 
     // Get Vehicle type from user
     cout << "What is your vehicle type? (regular, motorcycle, or lowEmssion?): ";
-    cin >> vehicleType;
+    getline(cin, vehicleType);
     // Loop until valid input is recieved
     while (vehicleType != "regular" && vehicleType != "motorcycle" && vehicleType != "lowEmission") {
         cout << "Please try again (input is case sensitive): ";
-        cin >> vehicleType;
+        getline(cin, vehicleType);
     }
 
     if (vehicleType == "regular") {
@@ -117,15 +117,15 @@ int main() {
 
         // Get input from user
         cout << "Enter vehicle make: ";
-        cin >> make;
+        getline(cin, make);
         cout << "Enter vehicle model: ";
-        cin >> model;
+        getline(cin, model);
         cout << "Enter vehicle year: ";
-        cin >> year;
+        getline(cin, year);
         cout << "Enter vehicle color: ";
-        cin >> color;
+        getline(cin, color);
         cout << "Enter license plate: ";
-        cin >> licensePlate;
+        getline(cin, licensePlate);
         
         // Create Regular object
         regular1 = Regular(make, model, year, color, licensePlate);
@@ -135,15 +135,15 @@ int main() {
 
         // Get input from user
         cout << "Enter vehicle make: ";
-        cin >> make;
+        getline(cin, make);
         cout << "Enter vehicle model: ";
-        cin >> model;
+        getline(cin, model);
         cout << "Enter vehicle year: ";
-        cin >> year;
+        getline(cin, year);
         cout << "Enter CCs: ";
-        cin >> cc;
+        getline(cin, cc);
         cout << "Enter capacity: ";
-        cin >> capacity;
+        getline(cin, capacity);
 
         // Create Motorcycle Object
         motorcycle1 = Motorcycle(make, model, year, cc, capacity);
@@ -153,15 +153,15 @@ int main() {
 
         // Get input from user
         cout << "Enter vehicle make: ";
-        cin >> make;
+        getline(cin, make);
         cout << "Enter vehicle model: ";
-        cin >> model;
+        getline(cin, model);
         cout << "Enter vehicle year: ";
-        cin >> year;
+        getline(cin, year);
         cout << "Enter MPG: ";
-        cin >> mpg;
+        getline(cin, mpg);
         cout << "Enter weight in pounds: ";
-        cin >> weight;
+        getline(cin, weight);
 
         // Create LowEmission object
         lowEmission1 = LowEmission(make, model, year, mpg, weight);
@@ -170,11 +170,11 @@ int main() {
 
     // Get Permit type from user
     cout << "What type of permit would you like to purchase? (annual, semester, or one-day?): ";
-    cin >> permitType;
+    getline(cin, permitType);
     // Loop until valid input is recieved
     while (permitType != "annual" && permitType != "semester" && permitType != "one-day") {
         cout << "Please try again (input is case sensitive): ";
-        cin >> permitType;
+        getline(cin, permitType);
     }
     
     // Yes I KNOW this is a lot of hard coded logic but idk how else to determine which objects to pass
@@ -218,27 +218,27 @@ int main() {
 
         if (customerType == "visitor") { // Case for visitor
             if (vehicleType == "regular") {
-                semester.printInvoice(visitor1, regular1);
+                cout << semester.printInvoice(visitor1, regular1);
             } else if (vehicleType == "motorcycle") {
-                semester.printInvoice(visitor1, motorcycle1);
+                cout << semester.printInvoice(visitor1, motorcycle1);
             } else if (vehicleType == "lowEmission") {
-                semester.printInvoice(visitor1, lowEmission1);
+                cout << semester.printInvoice(visitor1, lowEmission1);
             }
         } else if (customerType == "student") { // Case for student
             if (vehicleType == "regular") {
-                semester.printInvoice(student1, regular1);
+                cout << semester.printInvoice(student1, regular1);
             } else if (vehicleType == "motorcycle") {
-                semester.printInvoice(student1, motorcycle1);
+                cout << semester.printInvoice(student1, motorcycle1);
             } else if (vehicleType == "lowEmission") {
-                semester.printInvoice(student1, lowEmission1);
+                cout << semester.printInvoice(student1, lowEmission1);
             }
         } else if (customerType == "employee") { // Case for employee
             if (vehicleType == "regular") {
-                semester.printInvoice(employee1, regular1);
+                cout << semester.printInvoice(employee1, regular1);
             } else if (vehicleType == "motorcycle") {
-                semester.printInvoice(employee1, motorcycle1);
+                cout << semester.printInvoice(employee1, motorcycle1);
             } else if (vehicleType == "lowEmission") {
-                semester.printInvoice(employee1, lowEmission1);
+                cout << semester.printInvoice(employee1, lowEmission1);
             }
         }
     } else if (permitType == "one-day") {
@@ -247,27 +247,27 @@ int main() {
 
         if (customerType == "visitor") { // Case for visitor
             if (vehicleType == "regular") {
-                oneDay.printInvoice(visitor1, regular1);
+                cout << oneDay.printInvoice(visitor1, regular1);
             } else if (vehicleType == "motorcycle") {
-                oneDay.printInvoice(visitor1, motorcycle1);
+                cout << oneDay.printInvoice(visitor1, motorcycle1);
             } else if (vehicleType == "lowEmission") {
-                oneDay.printInvoice(visitor1, lowEmission1);
+                cout << oneDay.printInvoice(visitor1, lowEmission1);
             }
         } else if (customerType == "student") { // Case for student
             if (vehicleType == "regular") {
-                oneDay.printInvoice(student1, regular1);
+                cout << oneDay.printInvoice(student1, regular1);
             } else if (vehicleType == "motorcycle") {
-                oneDay.printInvoice(student1, motorcycle1);
+                cout << oneDay.printInvoice(student1, motorcycle1);
             } else if (vehicleType == "lowEmission") {
-                oneDay.printInvoice(student1, lowEmission1);
+                cout << oneDay.printInvoice(student1, lowEmission1);
             }
         } else if (customerType == "employee") { // Case for employee
             if (vehicleType == "regular") {
-                oneDay.printInvoice(employee1, regular1);
+                cout << oneDay.printInvoice(employee1, regular1);
             } else if (vehicleType == "motorcycle") {
-                oneDay.printInvoice(employee1, motorcycle1);
+                cout << oneDay.printInvoice(employee1, motorcycle1);
             } else if (vehicleType == "lowEmission") {
-                oneDay.printInvoice(employee1, lowEmission1);
+                cout << oneDay.printInvoice(employee1, lowEmission1);
             }
         }
     }
