fe9db9a4542994e860400d954012b5aa3d4f5d55
D-Guy Laptop <ddcool535@gmail.com>
2024-02-29 10:31:58 -0500

    Added function to calc sequence

---
+++ lecture/week8/3np1_IMPROVED.cpp
@@ -0,0 +1,97 @@
+// here are the required function signatures
+// Gradescope will not work without them all included
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// isOdd
+// Returns true if value is even, returns false otheriwise 
+bool isOdd(int n) {
+    if (n % 2 == 1) {
+        return true; // == 1
+    } else {
+        return false; // == 0
+    }
+}
+
+// get3nplus1
+// Computes the sequence of 3n+1
+vector<int> get3nplus1(int value) {
+    vector<int> sequence;
+    int current = value;
+
+    sequence.emplace_back(value);
+
+    // While the current value of the seqeuence != 1, continute the sequence
+    while (current != 1) {
+        if (isOdd(current)) {
+            current = (current * 3) + 1; // If odd, triple and add 1
+        } else {
+            current = (current / 2); // If even, divide by 2
+        }
+        sequence.emplace_back(current); // Add the current value of the sequence
+    }
+
+    return sequence;
+}
+
+// len3nplus1
+// Returns the number of elements in the 3n+1 sequence of n
+int len3nplus1(int value) {
+    vector<int> s;
+    int count = 0;
+
+    s = get3nplus1(value);
+
+    count = s.size();
+
+    return count;
+}
+
+// seumSeq3nPlus1
+// Returns the sum of the 3n+1 sequence
+int sumSeq3nPlus1(int value) {
+    vector<int> s;
+    int sum = 0;
+
+    s = get3nplus1(value);
+
+    // Range based for loop to get sum
+    for (int& n : s) {
+        sum += n;
+    }
+
+    return sum;
+}
+
+// longestSequence
+// returns the number in the range from 1 to max that has the longest 3n+1 sequence
+// if there is more than 1, return the first one. 
+int longestSequence(int max) {
+    vector<int> sequenceLengths;
+    int largest, position;
+
+    for (int i = 1; i <= max; i++) {
+        sequenceLengths.emplace_back(len3nplus1(i));
+    }
+
+    // Get the position of the largest element
+    position = distance(begin(sequenceLengths), max_element(begin(sequenceLengths), end(sequenceLengths)));
+
+    // +1 to account for index starting at 0
+    return position + 1;
+}
+
+int main() {
+    cout << len3nplus1(5) << endl;
+
+    cout << sumSeq3nPlus1(5) << endl;
+    cout << sumSeq3nPlus1(31) << endl;
+
+    cout << longestSequence(5) << endl;
+
+    return 0;
+}
