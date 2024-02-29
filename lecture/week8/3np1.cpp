1ff6735dff7a36467f79f8cbfaa058fbd2cc002f
D-Guy Laptop <ddcool535@gmail.com>
2024-02-29 10:07:27 -0500

    Completed B code

--- lecture/week8/3np1.cpp
+++ lecture/week8/3np1.cpp
@@ -19,10 +19,10 @@ bool isOdd(int n) {
 // Returns the number of elements in the 3n+1 sequence of n
 int len3nplus1(int value) {
     vector<int> sequence;
-    int current, count;
+    int current = value;
+    int count = 0;
 
     sequence.emplace_back(value);
-    current = value;
 
     // While the current value of the seqeuence != 1, continute the sequence
     while (current != 1) {
@@ -42,7 +42,28 @@ int len3nplus1(int value) {
 // seumSeq3nPlus1
 // Returns the sum of the 3n+1 sequence
 int sumSeq3nPlus1(int value) {
+    vector<int> sequence;
+    int current = value;
+    int sum = 0;
 
+    sequence.emplace_back(value);
+
+    // While the current value of the seqeuence != 1, continute the sequence
+    while (current != 1) {
+        if (isOdd(current)) {
+            current = (current * 3) + 1; // If odd, triple and add 1
+        } else {
+            current = (current / 2); // If even, divide by 2
+        }
+        sequence.emplace_back(current); // Add the current value of the sequence
+    }
+
+    // Range based for loop to get sum
+    for (int& n : sequence) {
+        sum += n;
+    }
+
+    return sum;
 }
 
 // longestSequence
