ae267185f4bff99a07053d816918ff4d46535582
D-Guy Laptop <ddcool535@gmail.com>
2024-02-29 09:46:59 -0500

    Completed D code

---
+++ lecture/week8/3np1.cpp
@@ -0,0 +1,37 @@
+// here are the required function signatures
+// Gradescope will not work without them all included
+
+#include <vector>
+
+using namespace std;
+
+// isOdd
+// Returns true if value is even, returns false otheriwise 
+bool isOdd(int n) {
+    if (n % 2 == 1) {
+        return true; // == 1
+    } else {
+        return false; // == 0
+    }
+}
+
+// len3nplus1
+// Returns the number of elements in the 3n+1 sequence of n
+int len3nplus1(int value) {
+
+
+    return count + 1;
+}
+
+// seumSeq3nPlus1
+// Returns the sum of the 3n+1 sequence
+int sumSeq3nPlus1(int value) {
+
+}
+
+// longestSequence
+// returns the number in the range from 1 to max that has the longest 3n+1 sequence
+// if there is more than 1, return the first one. 
+int longestSequence(int max) {
+
+}
