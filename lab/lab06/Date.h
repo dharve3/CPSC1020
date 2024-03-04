effe88c83e572f5517643c0af8bb603dd7215d84
D-Guy Laptop <ddcool535@gmail.com>
2024-03-04 08:23:37 -0500

    Added inital headers

---
+++ lab/lab06/Date.h
@@ -0,0 +1,7 @@
+// Name: Dylan Harvey
+// Date: 3/8/2024
+// Class: CPSC 1021
+// Lab Name: Lab06 - Date Class with I/O
+// File Name: Date.h
+// Desc: 
+
---
+++ lab/lab06/main.cpp
@@ -0,0 +1,10 @@
+// Name: Dylan Harvey
+// Date: 3/8/2024
+// Class: CPSC 1021
+// Lab Name: Lab06 - Date Class with I/O
+// File Name: main.cpp
+// Desc: 
+
+int main(int argc, char const *argv[]) {
+
+}
