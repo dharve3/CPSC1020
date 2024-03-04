5308f25ae627cb301f4a00a491bf3e1755f5baed
D-Guy Laptop <ddcool535@gmail.com>
2024-03-04 09:35:16 -0500

    Completed Date.h

--- lab/lab06/Date.h
+++ lab/lab06/Date.h
@@ -5,3 +5,34 @@
 // File Name: Date.h
 // Desc: 
 
+#ifndef DATE_H
+#define DATE_H
+
+#include <string>
+#include <sstream> // Stringstream
+
+using namespace std;
+
+class Date {
+private:
+    int month {1};
+    int day {1};
+    int year {1900};
+    const static string MONTHS[12] {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+
+public:
+    Date() = default;
+    Date(int m, int d, int y) : month(m), day(d), year(y) {}
+
+    int getMonth() const;
+    void setMonth(int m);
+    int getDay() const;
+    void setDay(int d);
+    int getYear() const;
+    void setYear(int y);
+    
+    string print();
+    static bool compare(const Date d1, const Date d2);
+}
+
+#endif // DATE_H
