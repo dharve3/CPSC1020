911e2a714866bc26cb27afe6e92b02510185f8bb
D-Guy Laptop <ddcool535@gmail.com>
2024-03-05 16:33:41 -0500

    Started Date.cpp

--- lab/lab06/Date.cpp
+++ lab/lab06/Date.cpp
@@ -7,6 +7,28 @@
 
 #include "Date.h"
 
-const int Date::getMonth() {
+int Date::getMonth() const {
+    return month;
+}
+void Date::setMonth(int m) {
+    month = m;
+}
+int Date::getDay() const {
+    return day;
+}
+void Date::setDay(int d) {
+    day = d;
+}
+int Date::getYear() const {
+    return year;
+}
+void Date::setYear(int y) {
+    year = y;
+}
+
+string Date::print() {
     
+}
+static bool compare(const Date d1, const Date d2) {
+
 }
