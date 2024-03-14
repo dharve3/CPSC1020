3eceeb457b5c118346b473c76a7162cccecaa6d7
D-Guy Laptop <ddcool535@gmail.com>
2024-03-14 08:58:56 -0500

    Updated copyConstructor example

--- lecture/week10/MidEx2/20CopyConstructor.cpp
+++ lecture/week10/MidEx2/20CopyConstructor.cpp
@@ -4,7 +4,7 @@ class MyClass {
 public:
     int value;
     MyClass(int val) : value(val) {}
-    // Copy constructor
+    // Copy constructor:
     MyClass(const MyClass& obj) {
         value = obj.value;
     }
@@ -16,3 +16,15 @@ int main() {
     std::cout << "Value of obj2: " << obj2.value << std::endl; // Output: 5
     return 0;
 }
+
+
+// Example from textbook:
+class CpClass {
+    int *p; // Private ptr p
+public:
+    CpClass(const CpClass &obj) // Copy Constructor
+    { p = new int; *p = *obj.p; }
+    CpClass(int v=0) // Normal Constructor
+    { p = new int; *p = v; }
+    ~CpClass(){delete p;} // Destructor
+};
