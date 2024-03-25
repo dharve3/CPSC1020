3afb45dfdb702dfca003ef9d230add22be88332e
D-Guy Desktop <ddcool535@gmail.com>
2024-03-25 15:19:15 -0400

    Added file headers

--- lab/lab08/Shape.h
+++ lab/lab08/Shape.h
@@ -1,3 +1,10 @@
+// Name: Dylan Harvey
+// Date: 3/25/2024
+// Class: CPSC 1021
+// Lab Name: Lab08 - Inheritance and Polymorphism
+// File Name: Shape.h
+// Desc: Headerfile for Shape class, parent class of many others
+
 #ifndef SHAPE_H_
 #define SHAPE_H_
 
--- lab/lab08/main.cpp
+++ lab/lab08/main.cpp
@@ -1,3 +1,10 @@
+// Name: Dylan Harvey
+// Date: 3/25/2024
+// Class: CPSC 1021
+// Lab Name: Lab08 - Inheritance and Polymorphism
+// File Name: main.cpp
+// Desc: Showcases derived classes and objects using inheritance and polymorphism
+
 #include "Shape.h"
 #include "Rectangle.h"
 #include "Square.h"
@@ -21,13 +28,10 @@ int main()
     // make an object named 'trapezoid' from class Trapezoid with base1 initialized to 2.5, base2 to 3.5, and height to 4.0
     Trapezoid trapezoid(2.5, 3.5, 4.0);
 
-    
     // make a vector 'vector<Type>shapeVect {element0, element1, element2, element3}'
     vector<Shape*>shapeVect {&circle, &square, &rectangle, &trapezoid};
     // where 'Type' is a pointer to class Shape and each element is the memory address
     // of one of objects you have instantiated above
-
-
     
     // use a range-based for loop to print the name and area of each object using
     // functions printName() and getArea() accessed through the base class pointer
