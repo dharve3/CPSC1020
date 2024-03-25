d61512478e770ae6fe6814dfc64ad79faae91abe
D-Guy Laptop <ddcool535@gmail.com>
2024-03-25 09:45:24 -0400

    Added Circle Class

--- lab/lab08/Circle.cpp
+++ lab/lab08/Circle.cpp
@@ -1,4 +1,15 @@
-#include "Circle.h"
+// Angie Diaz
+// CPSC 1021
+// File: Circle.cpp
+// Lab 8
+// Date:3/25/24
 
+#include "Circle.h"
+#include <cmath>
 // implement getArea()
+
+// Returns area of circle
+double Circle::getArea() {
+    return (PI * pow(radius, 2))
+}
 
--- lab/lab08/Circle.h
+++ lab/lab08/Circle.h
@@ -1,10 +1,27 @@
+// Angie Diaz
+// CPSC 1021
+// File: Circle.h
+// Lab 8
+// Date: 3/25/24
+
 #ifndef CIRCLE_H_
 #define CIRCLE_H_
 
 #include "Shape.h"
 
-class Circle // inherit publically from Shape
+class Circle: public Shape // inherit publically from Shape
 {
+    private:
+        const double PI=3.14;
+        double radius;
+    public: 
+        //default constructor
+        Circle();
+        //argument constructor 
+        Circle(double r): radius(r), name("Circle") {}
+        //abstract function 
+        double getArea();
+
 
 };
 
