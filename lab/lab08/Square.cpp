ff6556eba7fb33ac16cdb8ec6f748f64ba1d6007
jbhosal <157389465+jbhosal@users.noreply.github.com>
2024-03-25 09:47:23 -0400

    Update Square.cpp

--- lab/lab08/Square.cpp
+++ lab/lab08/Square.cpp
@@ -1,4 +1,14 @@
+// Janaki Bhosale
+// CPSC1021
+// Date: March 25, 2024
+// Description: Source file for square
+// Time: 5 minutes
+
 #include "Square.h"
 
 // implement getArea()
 
+double Square::getArea()
+{
+    return (side * side);
+}
