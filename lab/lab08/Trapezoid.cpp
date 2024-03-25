00a5f357e5cbdc613eba703a62fd1e41a43c45b8
D-Guy Laptop <ddcool535@gmail.com>
2024-03-25 09:54:33 -0400

    Added Trapezoid

--- lab/lab08/Trapezoid.cpp
+++ lab/lab08/Trapezoid.cpp
@@ -1,4 +1,14 @@
+// Angie Diaz
+// CPSC 1021
+// File: Circle.cpp
+// Lab 8
+// Date: 3/25/2024
+
 #include "Trapezoid.h"
 
 // implement getArea()
 
+double Trapezoid::getArea() {
+    //returns area of trapezoid 
+    return ((base1 + base2) * (height / 2));
+}
--- lab/lab08/Trapezoid.h
+++ lab/lab08/Trapezoid.h
@@ -1,10 +1,27 @@
+// Angie Diaz
+// CPSC 1021
+// File: Circle.cpp
+// Lab 8
+// Date: 3/25/2024
+
 #ifndef TRAPEZOID_H_
 #define TRAPEZOID_H_
 
 #include "Shape.h"
 
-class Trapezoid  // inherit publically from Shape
+class Trapezoid: public Shape  // inherit publically from Shape
 {
+    private:
+        double base1;
+        double base2;
+        double height;
+    public: 
+        //default constructor
+        Trapezoid();
+        //argument constructor
+        Trapezoid(double b1, double b2, double h): Shape("Trapezoid"), base1(b1), base2(b2), height(h) {}
+        //abstract function
+        double getArea();
 };
 
 #endif
