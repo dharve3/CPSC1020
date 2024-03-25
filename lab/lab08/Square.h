53487741a4a37b19aac41f90adb64c80a6647e99
jbhosal <157389465+jbhosal@users.noreply.github.com>
2024-03-25 09:47:07 -0400

    Update Square.h

--- lab/lab08/Square.h
+++ lab/lab08/Square.h
@@ -1,10 +1,28 @@
+// Janaki Bhosale
+// CPSC1021
+// Date: March 25, 2024
+// Description: Header file for square
+// Time: 5 minutes
+
 #ifndef SQUARE_H_
 #define SQUARE_H_
 
 #include "Shape.h"
 
-class Square // inherit publically from Shape
+class Square::public Shape // inherit publically from Shape
 {
+    private:
+        double side;
+    public:
+        // default constructor
+        Square();
+
+        // argument constructor
+        Square(double s): side{s}, name("Square") {}
+
+        // abstract function
+        double getArea();
+
 };
 
 #endif
