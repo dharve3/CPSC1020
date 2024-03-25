9c43ea81a255dfd2f6db85d22f70d61557f6ff4f
jbhosal <157389465+jbhosal@users.noreply.github.com>
2024-03-25 09:46:15 -0400

    Update Rectangle.cpp

--- lab/lab08/Rectangle.cpp
+++ lab/lab08/Rectangle.cpp
@@ -1,4 +1,29 @@
-#include "Rectangle.h"
+// Janaki Bhosale
+// CPSC1021
+// Date: March 25, 2024
+// Description: Header file for rectangle
+// Time: 5 minutes
 
-// implement getArea()
+#ifndef RECTANGLE_H_
+#define RECTANGLE_H_
+
+#include "Shape.h"
+
+class Rectangle::public Shape // inherit publically from Shape
+{
+    private:
+        double length;
+        double width;
+    public:
+        // default constructor
+        Rectangle();
+
+        // argument constructor
+        Rectangle(double l, double w): length{l}, width{w}, name("Rectangle") {}
+
+        // abstract function
+        double getArea();
+};
+
+#endif
 
