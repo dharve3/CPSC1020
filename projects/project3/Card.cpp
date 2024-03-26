bad6832651aa421d7d93a87a62d7d72ef8cd8fb5
D-Guy Laptop <ddcool535@gmail.com>
2024-03-26 10:47:30 -0400

    Updated Card comments

--- projects/project3/Card.cpp
+++ projects/project3/Card.cpp
@@ -1,17 +1,17 @@
 #include "Card.h"
 
+// Returns formatted string for displaying a card
 std::string Card::strCard() {
     return (getColor() + ":" + std::to_string(getRank()));
 }
 
+// Accessors
 int Card::getRank() {
     return rank;
 }
-
 Card::Color Card::getColor() {
     return color;
 }
-
 int Card::getValue() {
     return value;
 }
