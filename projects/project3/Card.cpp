f2292900a50d7cda6742403179f3908458231500
D-Guy Laptop <ddcool535@gmail.com>
2024-03-26 10:16:04 -0400

    Wrote Class Card

--- projects/project3/Card.cpp
+++ projects/project3/Card.cpp
@@ -1,2 +1,17 @@
 #include "Card.h"
 
+std::string Card::strCard() {
+    return (getColor() + ":" + std::to_string(getRank()));
+}
+
+int Card::getRank() {
+    return rank;
+}
+
+Card::Color Card::getColor() {
+    return color;
+}
+
+int Card::getValue() {
+    return value;
+}
