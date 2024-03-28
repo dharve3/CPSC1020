301fceadeb3c93ce6c7c07082442126912307e73
D-Guy Desktop <ddcool535@gmail.com>
2024-03-27 20:28:08 -0400

    Updated Player class

--- projects/project3/Player.h
+++ projects/project3/Player.h
@@ -12,37 +12,22 @@ class Player
 {
 public:
     // Constructors
-    Player(); // Default constructor
-    Player(Deck& deck, int N); // Parameterized constructor to initialize hand with N cards
+    Player() = default;
+    Player(Deck& deck, int N): hand(deck, N) {} // Parameterized constructor to initialize hand with N cards
     // Getters
-    const Hand& getHand();
-    int getScore();
+    Hand& getHand() {
+        return hand;
+    }
+    int getScore() {
+        return score;
+    }
     // Setters
-    void setScore(int newScore);
+    void setScore(int newScore) {
+        score = newScore;
+    }
 private:
     Hand hand;
     int score {0}; // Initialize score to 0 using in-class initialization
 };
-
-// Default constructor definition
-inline Player::Player() {}
-
-// Parameterized constructor definition
-inline Player::Player(Deck& deck, int N) : hand(deck, N) {}
-
-// Getter for hand
-inline const Hand& Player::getHand() const {
-    return hand;
-}
-
-// Getter for score
-inline int Player::getScore() const {
-    return score;
-}
-
-// Setter for score
-inline void Player::setScore(int newScore) {
-    score = newScore;
-}
 
 #endif // PLAYER_H_
