017399a0fafdc3f3a2ea3ae18a2da13d4d5dbe42
D-Guy Desktop <ddcool535@gmail.com>
2024-03-27 20:18:20 -0400

    Created Hand class

--- projects/project3/Hand.cpp
+++ projects/project3/Hand.cpp
@@ -1,2 +1,27 @@
 #include "Hand.h"
 
+// Parameterized constructor to draw N cards from the deck
+Hand::Hand(Deck deck, int N) {
+    for (int i = 0; i < N; i++) {
+        hand.push_back(deck.drawCard());
+    }
+}
+
+// Shows the current hand
+std::string Hand::strHand() {
+    std::string handStr;
+    for (size_t i = 0; i < hand.size(); ++i) {
+        handStr += "[" + std::to_string(i + 1) + "] " + hand[i].strCard() + " ";
+    }
+    return handStr;
+}
+// Deals a card from the hand based on its position
+Card Hand::dealCard(int num) {
+    Card dealtCard = hand[num - 1]; // Adjusting for 1-based indexing
+    hand.erase(hand.begin() + num - 1);
+    return dealtCard;
+}
+// Returns the current number of cards in the hand
+int Hand::getHandSize() const {
+    return hand.size();
+}
--- projects/project3/Hand.h
+++ projects/project3/Hand.h
@@ -1,13 +1,23 @@
 #ifndef HAND_H_
 #define HAND_H_
 
+#include "Card.h"
 #include <vector>
 #include <string>
 #include <sstream>
 
 class Hand
 {
-  
+public:
+    // Constructors
+    Hand() = default;
+    Hand(Deck deck, int N);
+
+    std::string strHand();
+    Card dealCard(int num);
+    int getHandSize();
+private:
+    std::vector<Card> hand;
 };
 
 #endif // HAND_H_
