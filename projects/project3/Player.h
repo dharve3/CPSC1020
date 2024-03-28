a2cfd87df278ec627793cd5809a3d9c80b5419b1
D-Guy Desktop <ddcool535@gmail.com>
2024-03-27 20:26:06 -0400

    Wrote Player class

--- projects/project3/Player.h
+++ projects/project3/Player.h
@@ -5,10 +5,44 @@
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
+#include "Hand.h"
+#include "Deck.h"
+
 class Player
 {
-
+public:
+    // Constructors
+    Player(); // Default constructor
+    Player(Deck& deck, int N); // Parameterized constructor to initialize hand with N cards
+    // Getters
+    const Hand& getHand();
+    int getScore();
+    // Setters
+    void setScore(int newScore);
+private:
+    Hand hand;
+    int score {0}; // Initialize score to 0 using in-class initialization
 };
-  
+
+// Default constructor definition
+inline Player::Player() {}
+
+// Parameterized constructor definition
+inline Player::Player(Deck& deck, int N) : hand(deck, N) {}
+
+// Getter for hand
+inline const Hand& Player::getHand() const {
+    return hand;
+}
+
+// Getter for score
+inline int Player::getScore() const {
+    return score;
+}
+
+// Setter for score
+inline void Player::setScore(int newScore) {
+    score = newScore;
+}
 
 #endif // PLAYER_H_
