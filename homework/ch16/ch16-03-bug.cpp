09356200187a738475c0a4b02bd791ca9d73e483
D-Guy Desktop <ddcool535@gmail.com>
2024-04-03 15:18:19 -0400

    Fixed ch16-03-bug.cpp

--- homework/ch16/ch16-03-bug.cpp
+++ homework/ch16/ch16-03-bug.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 template <class T>
-T sum(int val1, int val2)
+T sum(T val1, T val2) // Fixed to type T not int
 {
     return val1 + val2;
 }
 
-template <class X>
+template <class T> // Fixed class X to T
 T sum(T val1, T val2, T val3)
 {
     return val1 + val2 + val3;
@@ -19,7 +19,7 @@ T sum(T val1, T val2, T val3)
 int main()
 {
     cout << "The sum of 2 doubles is: " << sum(45.7, 22.5) << endl;
-    cout << "The sum of 2 integers is: " << sum(5,10) << endl;
+    cout << "The sum of 2 integers is: " << sum(5, 10) << endl;
     cout << "The sum of 3 integers is: " << sum(1, 2, 3) << endl;
     cout << "The sum of 3 doubles is: " << sum(3.1418, 3.1418, 3.1418) << endl;
 
