ebe76e6e55c965a437cefbb5712460f10c306861
D-Guy Laptop <ddcool535@gmail.com>
2024-04-10 09:18:18 -0400

    Completed lab10/3 sets

--- lab/lab10/3Sets/main.cpp
+++ lab/lab10/3Sets/main.cpp
@@ -25,6 +25,7 @@ int main() {
 
     // Display the unique elements in the new vector using a single space to
     // separate each one. Use an endl at the end of the list.
+    cout << "Unique elements in the new vector: "; 
     for (const auto& s : uniqueStrings) {
         cout << s << " ";
         // NOTE: may need to remove the tailing " "
