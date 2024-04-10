2950feb22085f1dd92f14e1c7bae18592d2a26c0
D-Guy Laptop <ddcool535@gmail.com>
2024-04-10 09:11:25 -0400

    Completed lab10/1 maps

--- lab/lab10/1Maps/main.cpp
+++ lab/lab10/1Maps/main.cpp
@@ -11,7 +11,7 @@ out and thereby learn maps. */
 #include <sstream>
 
 int main() {
-    std::string sentence = "Remember I have been prophesying for the past sixty five years that in the natural course of things I a square a mere Square shall evolve into a Pentagon or five sided figure and from there into a Hexagon or six sided figure and thence into a Cube or in other words into the so called regular solids with a myriad of various other forms of which the chief are the tetrahedron the octahedron and the dodecahedron";
+    std::string sentence = "In the beginning was the Word and the Word was with God and the Word was God the same was in the beginning with God";
 
     // [declare a map which consists of a string as key and int as value, called wordcount]
     std::map<std::string, int> wordCount;
