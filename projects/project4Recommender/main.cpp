5869df2571bb8ee849cf50fdcd376d2cc7d33ce5
D-Guy Laptop <ddcool535@gmail.com>
2024-04-18 10:11:13 -0400

    Updated main.cpp commands

--- projects/project4Recommender/main.cpp
+++ projects/project4Recommender/main.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
 #include "Recommend.h"
 
-int main() {
+// Command: averages - display average ratings for each book
+
+// Command: books - display list of books
+
+// Command: debug - toggles debugging outputs
+
+// Command: dotprod - display current dot products
+
+// Command: file - display ratings in fancy format
+
+// Command: menu - display menu of commands
+
+// Command: names - display list of recommender names
+
+// Command: ratings - display ratings
+
+// Command: recommend <name> - display book recommendations based upon recommender <name>
+
+// Command: similar - display current similarities
+
+// Command: simavg - display averages for current similarities
+
+// Command: quit - exits the program
+
+int main(int argc, char* argv[]) {
+    // Check for the correct number of command line arguments
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <filename>" << endl;
+        return 1;
+    }
+
 	// File name containing recommendations data
-	std::string fileName = "ratings_small.dat";
+	string fileName = "ratings_small.dat";
 
 	// Create an instance of Recommend class
 	Recommend recommender(fileName);
 
+	string choice;
+	// Menu Loop
+	while (true) {
+		getline(cin, choice);
+	}
+
 	// Print total number of books and recommenders
-	std::cout << "Total number of books: " << recommender.getBookCount() << std::endl;
-	std::cout << "Total number of recommenders: " << recommender.getRecommenderCount() << std::endl;
+	cout << "Total number of books: " << recommender.getBookCount() << endl;
+	cout << "Total number of recommenders: " << recommender.getRecommenderCount() << endl;
 
 	// Compute recommendation for a specific recommender
 	recommender.computeRecommendation("Bob");
