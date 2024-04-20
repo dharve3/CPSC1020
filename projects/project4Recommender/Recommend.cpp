0d1ac4a7ce540cd73f15d10ae5ef67871a9eb2e1
D-Guy <ddcool535@gmail.com>
2024-04-20 14:49:55 -0400

    Updated debug

--- projects/project4Recommender/Recommend.cpp
+++ projects/project4Recommender/Recommend.cpp
@@ -295,11 +295,11 @@ void Recommend::computeSimilarities(RECOMMENDER requester) {
             double dot_product = 0.0;
             // const vector<int>& recommenderRatings = ratings[recommender];
             for (size_t i = 0; i < books.size(); ++i) {
-                dot_product += ratings[requester][i] * ratings[recommender][i];
                 if (DEBUG) {
-                    cout << "DEBUG: " << "ratings[requester][i]: " << ratings[requester][i] << endl;
-                    cout << "DEBUG: " << "ratings[recommender][i]: " << ratings[recommender][i] << endl;
+                    cout << "DEBUG: " << "ratings[" << requester << "][" << i << "]: " << ratings[requester][i] << endl;
+                    cout << "DEBUG: " << "ratings[" << recommender << "][" << i << "]: " << ratings[recommender][i] << endl;
                 }
+                dot_product += ratings[requester][i] * ratings[recommender][i];
             }
             similarList.push_back(make_pair(recommender, dot_product));
         }
